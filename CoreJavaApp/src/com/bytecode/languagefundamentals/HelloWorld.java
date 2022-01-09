3b4278e474a38fad66f70fbf301b24b88437cfdd
LAPTOP-B1PN513I\hp <kayum.ascorp@gmail.com>
2022-01-09 21:05:48 +0530

    TEST

---
+++ CoreJavaApp/src/com/bytecode/languagefundamentals/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.bytecode.languagefundamentals;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("This is Hello World Program !!!");
+
+	}
+
+}
