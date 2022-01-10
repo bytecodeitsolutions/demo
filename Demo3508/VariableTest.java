af683ff57c7226f24832508cf07d84bc089aad74
LAPTOP-B1PN513I\hp <kayum.ascorp@gmail.com>
2022-01-10 08:51:49 +0530

    variabletest

---
+++ Demo3508/VariableTest.java
@@ -0,0 +1,26 @@
+class Test 
+{
+	static int  a;
+	int  b=20;
+	static Test  t1;
+	public static void main(String[] args) 
+	{
+		Test.a=100;
+		int c=30;
+		System.out.println("Static variable:: "+Test.a);
+        t1=new Test();
+		System.out.println("NonStatic variable:: "+t1.b);
+
+		System.out.println("Local variable:: "+c);
+          Test1.m1();
+
+	}
+}
+class Test1
+{
+	static void  m1(){
+
+		System.out.println("21-->Static variable:: "+Test.a);
+		System.out.println("23-->NonStatic variable:: "+t1.b);
+	}
+}
