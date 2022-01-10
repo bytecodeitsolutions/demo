1d4cbdbd011b10513fe948e0aaa4f93129e844ff
LAPTOP-B1PN513I\hp <kayum.ascorp@gmail.com>
2022-01-10 18:41:41 +0530

    updated demo1.java

--- CoreJavaApp/src/com/bytecode/languagefundamentals/Demo1.java
+++ CoreJavaApp/src/com/bytecode/languagefundamentals/Demo1.java
@@ -1,8 +1,13 @@
 package com.bytecode.languagefundamentals;
 
+/*
+ * This is multi -line comment
+ * */
 public class Demo1 {
 
 	public static void main(String[] args) {
+		 System.out.println("We are sending code to github");
+		 System.out.println("We are sending code to github");
 		 System.out.println("We are sending code to github");
 
 	}
