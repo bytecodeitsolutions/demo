4b53539257a8309b1d22160e3a7e14701d7a8db0
LAPTOP-B1PN513I\hp <kayum.ascorp@gmail.com>
2022-01-27 17:45:51 +0530

    code

--- Demo3508/VariableTest.java
+++ Demo3508/VariableTest.java
@@ -1,28 +1,19 @@
 class Test 
 {
-	static int  a;
-	int  b=20;
-	static Test  t1;
+	static Test  obj2=null;
+	static int a=10;
 	public static void main(String[] args) 
 	{
-		Test.a=100;
-		int c=30;
-		System.out.println("Static variable:: "+Test.a);
-       Test t1=new Test();
-		System.out.println("NonStatic variable:: "+t1.b);
 
-		System.out.println("Local variable:: "+c);
-          Test1.m1();
+        Test1 obj1=new Test1();
 
+		Test1.obj1.m1();
 	}
 }
 class Test1
 {
-	static void  m1(){
-
-		System.out.println("21-->Static variable:: "+Test.a);
-		Test  t2=new Test();
-		System.out.println("23-->NonStatic variable:: "+t2.b);
-		
+	static Test1 obj1=null;
+	 void  m1(){
+	    System.out.println("Test1--->m1()");
 	}
 }
---
+++ Demo3605/IdentifierDemo1.java
@@ -0,0 +1,11 @@
+class IdentifierDemo1 
+{
+	public static void main(String[] args) 
+	{
+        
+        int  Number=10;
+        int Runnable=20;
+		int String=100;
+		System.out.println(Runnable);
+	}
+}
